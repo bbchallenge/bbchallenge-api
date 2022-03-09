6afa0af48f2e56c943832154a13c970edd68317f
Cosmo <cosmo@vmi815784.contaboserver.net>
2022-03-09 12:38:33 +0100

    highlights

--- sync_redis_highlights.py
+++ sync_redis_highlights.py
@@ -17,12 +17,14 @@ highlighted_machines = [
     "40,899-halter;mAQACAAEDAAADAAAAAQAEAAAFAQEFAAABAAEBAAEF&s=40899&w=500&ox=0.93&status=halt",
     "20,739-halter;mAQACAAEBAAADAQAEAQEDAQEBAQABAQAFAAAAAAAC&s=20739&w=250&ox=0.93&status=halt",
     "7410754&s=10000&w=300&ox=0.5",
+    "55897188&s=10000&w=300&ox=0.5",
     "43374927&s=10000&w=300&ox=0.65",
     "27879939&s=10000&w=300&ox=0.5",
     "2394179&s=10000&w=300&ox=0.2",
     "2713328&s=10000&w=300&ox=0.5",
     '"chaotic" in [Marxen and Buntrock, 1990];76708232&s=10000&w=300&ox=0.3',
     '"complex counter" in [Marxen and Buntrock, 1990];10936909&s=5000&w=150&ox=0.5',
+
 ]
 
 for h in highlighted_machines:
