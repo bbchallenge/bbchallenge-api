408b17379b2d3d146c19bd896d6a32249222c435
Tristan Stérin <tristan.sterin@mu.ie>
2022-03-12 10:26:50 +0000

    highlights

--- sync_redis_highlights.py
+++ sync_redis_highlights.py
@@ -22,8 +22,8 @@ highlighted_machines = [
     "27879939&s=10000&w=300&ox=0.5",
     "62537065&s=10000&w=300&ox=0.5",
     "2713328&s=10000&w=300&ox=0.5",
-    '"chaotic" in [Marxen and Buntrock, 1990];76708232&s=10000&w=300&ox=0.3',
-    '"complex counter" in [Marxen and Buntrock, 1990];10936909&s=5000&w=150&ox=0.5',
+    '"chaotic" [Marxen & Buntrock, 1990];76708232&s=10000&w=300&ox=0.3',
+    '"complex counter" in [Marxen & Buntrock, 1990];10936909&s=5000&w=150&ox=0.5',
 
 ]
 
